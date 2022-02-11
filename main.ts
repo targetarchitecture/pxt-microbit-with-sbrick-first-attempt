113f56dde252ea3ed3321235e8a611eefcd7ded1
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2022-02-11 10:02:43 +0000

    Update main.ts

--- main.ts
+++ main.ts
@@ -17,9 +17,9 @@ bluetooth.onBluetoothDisconnected(function () {
         `)
 })
 input.onButtonPressed(Button.A, function () {
-    sbrick.drive(255, SBPort.A, SBDirection.Forward)
+    sbrick.drive(255, SBPort.C, SBDirection.Forward)
     basic.pause(1000)
-    sbrick.brake(SBPort.D)
+    sbrick.brake(SBPort.C)
 })
 sbrick.onConnected(function () {
     basic.showLeds(`
@@ -42,8 +42,8 @@ sbrick.connect("Duck")
 loops.everyInterval(100, function () {
     pressed = input.buttonIsPressed(Button.B)
     if (pressed) {
-        sbrick.drive(255, SBPort.A, SBDirection.Forward)
+        sbrick.drive(255, SBPort.D, SBDirection.Forward)
     } else {
-        sbrick.brake(SBPort.C)
+        sbrick.brake(SBPort.D)
     }
 })
